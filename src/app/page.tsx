bb7c5496d1bd37860fa64af8568e895b6a519949
nikhilgotcloud <nikhilanand73@gmail.com>
2024-04-10 08:29:59 +0530

    v1

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
+import Video from "@/components/Video";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rattan Energy",
+  description: "Rattan Energy",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      
+    
+      <Contact />
+    </>
+  );
+}
---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,144 @@
+"use client"
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://sheetdb.io/api/v1/312s52znczfko", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
+        toast.success("Form submitted successfully!", {
+          position: "top-right"
+        });
+        setFormData({
+          name: "",
+          email: "",
+          message: ""
+        });
+      } else {
+        // Handle error
+        console.error("Failed to submit data");
+      }
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+
+  return (
+    <>
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div
+              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              data-wow-delay=".15s" style={{'alignItems':'center'}}
+            >
+              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                Wanna Contact Us?
+              </h2>
+              <p className="mb-12 text-base font-medium text-body-color">
+                Fill this form and connect with us ASAP.
+              </p>
+              <form onSubmit={handleSubmit}>
+                <div className="-mx-4 flex flex-wrap">
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="name"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        placeholder="Enter your name"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="Enter your email"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="message"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Message
+                      </label>
+                      <textarea
+                        name="message"
+                        rows={5}
+                        value={formData.message}
+                        onChange={handleChange}
+                        placeholder="Enter your Message"
+                        className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <button type="submit" className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                      Submit 
+                    </button>
+                  </div>
+                </div>
+              </form>
+              
+            </div>
+          </div>
+          <div style={{'width':'100%'}}><iframe width="100%" height="600"  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Rattan%20Motors%20Sector%2023%20C%20Chandigarh+(Rattan%20Energy)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                </iframe></div>
+        </div>
+      </div>
+    </section>
+    <div className="py-6">
+            <p className="text-center text-base text-dark dark:text-white">
+            ©Copyright 2024. All rights reserved with Rattan Energy.
+              
+            </p>
+          </div>
+    </>
+  );
+};
+
+export default Contact;
